9c7960675eda9b738db6c70bc104004b2b62d58b
rya-sge <ryan.sauge@heig-vd.ch>
2021-06-19 10:40:40 +0200

    Mise en forme du code serveur tcp

--- src/auditor.js
+++ src/auditor.js
@@ -1,51 +1,59 @@
-/ Include Nodejs' net module.
-const Net = require('net');
-// The port on which the server is listening.
-/*
+// Include Nodejs' net module.
 
-TCP serveur sur l'auditeur
-Renvoie la liste des musciciens
-https://riptutorial.com/node-js/example/22405/a-simple-tcp-server
- */
 
-const port = 2205;
+
+
 /*class Musician{
     "uuid": string,
     "instrument":string,
     "activeSince": string
 }*/
+var config = require('./config.js');
 var musician = new Array();
 
-// Use net.createServer() in your code. This is just for illustration purpose.
-// Create a new TCP server.
-const auditor = new Net.Server();
-// The server listens to a socket for a client to make a connection request.
-// Think of a socket as an end point.
-auditor.listen(port, function() {
+
+
+
+
+
+
+/*
+TP serveur sur l'auditeur
+Renvoie la liste des musciciens
+https://riptutorial.com/node-js/example/22405/a-simple-tcp-server
+ */
+const Net = require('net');
+
+//Créer un nouveau serveur tcp
+exports.tcp_server = new Net.Server();
+
+
+//En attente de connexion
+tcp_server.listen(config.PORT_TCP, function() {
     console.log('Server listening for connection requests on socket localhost:${port}.');
 });
 
-// When a client requests a connection with the server, the server creates a new
-// socket dedicated to that client.
-auditor.on('connection', function(socket) {
+//A chaque requête de connexion de la part d'un client, le serveur créer un nouveau socket dédié au client
+tcp_server.on('connection', function(socket) {
     console.log('Une nouvelle connection a ét établie');
 
-    // Now that a TCP connection has been established, the server can send data to
-    // the client by writing to its socket.
+
+    //Envoie des données en écrivant dans le socket
     socket.write('Hello, client.');
     socket.write(JSON.stringify(musician));
     // The server can also receive data from the client by reading from its socket.
-   /* socket.on('data', function(chunk) {
-        console.log(`Data received from client: ${chunk.toString()`.});
-    });*/
+    /* socket.on('data', function(chunk) {
+         console.log(`Data received from client: ${chunk.toString()`.});
+     });*/
 
-    // When the client requests to end the TCP connection with the server, the server
-    // ends the connection.
+
+    //Le client demande à mettre fin à la connexion.
+    //le serveur met alors fin à la connexion
     socket.on('end', function() {
         console.log('Closing connection with the client');
     });
 
-    // Don't forget to catch error, for your own sake.
+    //On catch les erreurs
     socket.on('error', function(err) {
         console.log(`Error: ${err}`);
     });
