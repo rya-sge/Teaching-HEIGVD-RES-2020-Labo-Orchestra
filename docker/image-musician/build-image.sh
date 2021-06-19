cfbebb4706732a346b8befd35dd1f63bb9991471
rya-sge <ryan.sauge@heig-vd.ch>
2021-06-19 13:45:16 +0200

    Correction divers bug Muscien semble marcher

---
+++ docker/image-musician/build-image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# Build the Docker image locally
+docker build --tag udp_sauge_viotti/musicien .
--- src/musicien.js
+++ src/musicien.js
@@ -2,17 +2,18 @@
 https://www.npmjs.com/package/uuid
 
 */
-import { v4 as uuidv4 } from 'uuid';
+
 
 var config = require('./config.js');
 var dayjs = require('dayjs')
+var uuid = require('uuid')
 //import dayjs from 'dayjs' // ES 2015
 dayjs().format()
 
 //Class représentn les informations d'un muscien
 class mesInformations {
     constructor(sound) {
-        this.uuid = uuidv4();
+        this.uuid = uuid.v4();
         this.sound = sound;
         this.activeSince = dayjs();
     }
@@ -28,24 +29,27 @@ function play(mesInfos){
     const playload = JSON.stringify(mesInfos);
 
 //Peut-on le faier ???
-    const message = new Buffer(playload);
+    const message = Buffer.from(playload);
 
 //Pas correct => envoyer en multicast
-    server.send(message, config.PORT_UDP,test);
-    server.send(message, 0, message.length, config.PORT_UDP,config, config.MULTICAST_ADDRESS, function(err, bytes) {
-        console.log("Erreur : " + playload + server.address().port);
-        server.close();
+    server.send(message, 0, message.length, config.PORT_UDP, config.MULTICAST_ADDRESS, function(err, bytes) {
+        if(err){
+            console.log("Erreur ", err);
+            server.close();
+        }
+        console.log(playload + server.address().port);
     });
 
 }
 
 
 
-console.log("Le musicien comment à jouer...")
+
 if(config.instruments.get(process.argv[2]) === 'undefined'){
-    console.log("Erreur" + process.argv[2] )
+    console.log("Erreur pour l'instrument" + process.argv[2] )
 }else{
-    console.log(process.argv[2])
-    const mesInformations =  new mesInformations(config.instruments.get(process.argv[2]))
-    setInterval(play(mesInformations), config.TIME_PLAY);
+    console.log("Le musicien commence à jouer...")
+    console.log("Instrument : ", process.argv[2])
+    const infos =  new mesInformations(config.instruments.get(process.argv[2]))
+    setInterval(play, config.TIME_PLAY, infos);
 }
