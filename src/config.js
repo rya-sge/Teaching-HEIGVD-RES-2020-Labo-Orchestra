77510faf9cad1618b85a581196ea3ff9d318fa40
rya-sge <ryan.sauge@heig-vd.ch>
2021-06-19 12:01:26 +0200

    Ajout class mes Infos + interval pour joueur + récup argument + modif dockerfile

--- src/config.js
+++ src/config.js
@@ -3,17 +3,22 @@ Sources : https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
 exports permet d'exporter les variables
  */
 
-exports.myMap = new Map();
+
+//Copier du code de validation
+
+exports.instruments = new Map();
 // définir les valeurs
-myMap.set("piano", "ti-ta-ti");
-myMap.set("trumpet", "pouet");
-myMap.set("flute", "trulu");
-myMap.set("violin", "gzi-gzi");
-myMap.set("drum", "boum-boum");
+var instruments = new Map();
+instruments.set("piano", "ti-ta-ti");
+instruments.set("trumpet", "pouet");
+instruments.set("flute", "trulu");
+instruments.set("violin", "gzi-gzi");
+instruments.set("drum", "boum-boum");
 
 
 exports.PORT_TCP = 2205;
-exports.PORT_UDP = 1200;
+exports.PORT_UDP = 9907; //slide 53 UDP
 
 exports.TIME_END = 5; //secondes
-exports.TIME_PLAY = 1; //Secondes
+exports.TIME_PLAY = 1000; //mili
+exports.MULTICAST_ADDRESS="239.252.10.10";
--- src/musicien.js
+++ src/musicien.js
@@ -5,31 +5,47 @@ https://www.npmjs.com/package/uuid
 import { v4 as uuidv4 } from 'uuid';
 
 var config = require('./config.js');
+var dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
 
-
-var mesInformations = {
-    "uuid" : uuidv4(),
-    "sound" : config.myMap.get("piano"),
-    "activeSince" : "2016-04-27T05:20:50.731Z"
-};
+//Class représentn les informations d'un muscien
+class mesInformations {
+    constructor(sound) {
+        this.uuid = uuidv4();
+        this.sound = sound;
+        this.activeSince = dayjs();
+    }
+}
 
 //Fournir une implémentation des sockets UDP
 //https://nodejs.org/api/dgram.html
 //https://nodejs.org/dist./v0.10.44/docs/api/dgram.html
-const dgram = require('dgram');
+function play(mesInfos){
+    const dgram = require('dgram');
 
-const server = dgram.createSocket("udp4");
-const playload = JSON.stringify(mesInformations);
+    const server = dgram.createSocket("udp4");
+    const playload = JSON.stringify(mesInfos);
 
 //Peut-on le faier ???
-const message = new Buffer(playload);
+    const message = new Buffer(playload);
 
 //Pas correct => envoyer en multicast
-server.send(message, config.PORT_UDP,test);
-/*server.send(message, 0, message.length, 41234, "localhost", function(err, bytes) {
-    server.close();
-});*/
+    server.send(message, config.PORT_UDP,test);
+    server.send(message, 0, message.length, config.PORT_UDP,config, config.MULTICAST_ADDRESS, function(err, bytes) {
+        console.log("Erreur : " + playload + server.address().port);
+        server.close();
+    });
+
+}
 
 
 
 console.log("Le musicien comment à jouer...")
+if(config.instruments.get(process.argv[2]) == 'undefined'){
+    console.log("Erreur" + process.argv[2] )
+}else{
+    console.log(process.argv[2])
+    const mesInformations =  new mesInformations(config.instruments.get(process.argv[2]))
+    setInterval(play(mesInformations), config.TIME_PLAY);
+}
