c16bbf1087dc1c04cb8e919147d935aa99417e26
rya-sge <ryan.sauge@heig-vd.ch>
2021-06-20 11:32:49 +0200

    correction bug dans script

--- docker/image-musician/run-multiple.sh
+++ docker/image-musician/run-multiple.sh
@@ -6,5 +6,5 @@ sudo docker kill m4
 sudo docker system prune
 docker run --name="m1" -d res/musician piano
 docker run --name="m2" -d res/musician flute
-docker run --name="m3"- d res/musician flute
+docker run --name="m3" -d res/musician flute
 docker run --name="m4" -d res/musician drum
